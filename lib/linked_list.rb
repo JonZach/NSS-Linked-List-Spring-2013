fbae331a6ef00ef42f6b1ab990d71cfdb1eacd06
JonZach <Zach@yourgreenpal.com>
2013-04-23 09:26:10 -0500

    Ruby Linked List passing all tests except bonuses. Refactor complete.

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,18 +7,56 @@ class LinkedList
   end
 
   def add_item(payload)
+    next_item = LinkedListItem.new(payload)
+    if @first_item.nil?
+      @first_item = next_item
+    else
+    final_item = @first_item
+      while !final_item.last?
+        final_item = final_item.next_list_item
+      end
+      final_item.next_list_item = next_item
+    end
   end
 
   def get(n)
+    item = @first_item
+    n.times do
+      raise IndexError.new('item does not exist') if item.nil?
+      item = item.next_list_item
+    end
+    return item.payload
   end
 
   def last
+    item = @first_item
+    return nil if item.nil?
+    until item.last?
+      item = item.next_list_item
+    end
+    return item.payload
   end
 
   def size
+    item = @first_item
+    size = 0
+    until item.nil?
+      item = item.next_list_item
+      size += 1
+    end
+    size 
   end
 
   def to_s
+    current_item = @first_item
+    payloads = ""
+    while current_item
+      payloads += " "
+      payloads += current_item.payload
+      payloads += "," unless current_item.last?
+      current_item = current_item.next_list_item
+    end
+    "|#{payloads} |"
   end
 
   # ========= Bonus ========== #
--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -2,12 +2,24 @@ class LinkedListItem
   attr_reader :next_list_item
   attr_reader :payload
 
-  def initialize( payload )
+  def initialize( thepayload )
+  @payload = thepayload
   end
 
   def next_list_item= (linked_list_item)
+  if linked_list_item == self
+    raise ArgumentError.new('cannot equal itself')
+  end
+  @next_list_item = linked_list_item
   end
 
   def last?
+# ALSO CAN DO: next_list_item.nil?
+# can use instance variable w/@ or method w/no @ because has access to both
+  if next_list_item.nil?
+    true
+  else
+    false
+  end
   end
 end
